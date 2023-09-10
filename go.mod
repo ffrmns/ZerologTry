a406e146a3e340086026c53127e05e83636f15f5
Fikri Firmansyah Akbar <m-fikri@ysi.co.id>
2023-09-10 21:05:01 +0700

    chore: try panic with sql server connection

--- go.mod
+++ go.mod
@@ -5,7 +5,15 @@ go 1.21.1
 require github.com/rs/zerolog v1.30.0
 
 require (
+	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
+	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+)
+
+require (
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+	github.com/microsoft/go-mssqldb v1.6.0
+	golang.org/x/sys v0.11.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"math/rand"
+	"net/url"
 	"os"
 	"strconv"
 	"time"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+
+	_ "github.com/microsoft/go-mssqldb"
 )
 
 func main() {
@@ -24,8 +28,29 @@ func main() {
 	}
 
 	defer file.Close()
+
+	defer func() {
+		if r := recover(); r != nil {
+			log.Panic().Msgf("Panic: %v", r)
+		}
+	}()
 	multi := zerolog.MultiLevelWriter(zerolog.ConsoleWriter{Out: os.Stdout}, file)
 	log.Logger = log.Output(multi)
+
+	query := url.Values{}
+	query.Add("app name", "MyAppName")
+
+	u := &url.URL{
+		Scheme:   "sqlserver",
+		User:     url.UserPassword("sa", "password"),
+		Host:     fmt.Sprintf("%s:%d", "localhost", 1433),
+		RawQuery: query.Encode(),
+	}
+
+	db, err := sql.Open("sqlserver", u.String())
+	if err != nil {
+		panic(err)
+	}
 
 	if true {
 		log.Error().Msg("ERROR: Its true, so here the error!")
@@ -42,6 +67,11 @@ func main() {
 	year, month, day := time.Now().Date()
 	log.Debug().Msg(fmt.Sprintf("DEBUG: Now at %d-%d-%d", year, month, day))
 	log.Trace().Msg("TRACE: Your random number is " + strconv.Itoa(rand.Int()))
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
 }
 
 // Output:
